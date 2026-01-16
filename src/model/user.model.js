c8e0c5931cfe2458567fdc1422a2392adb5efd59
AndersonOls96 <11052@senacminas.edu.br>
2026-01-16 09:06:28 -0300

    Implementação da busca de usuário por ID

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -5,6 +5,8 @@ exports.findAll = () =>{
     return users
 }
 
+exports.findById = (id) => users.find(u => u.id === id)
+
 exports.create = (data) =>{
     const user = { id: id++, ...data }
     users.push(user)
