839a05c44cbd5b317e2de62b4e9f36ee88f7f1a7
AndersonOls96 <11052@senacminas.edu.br>
2026-01-20 11:15:55 +0000

    config e impl de views

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 const express = require('express')
-
 const userRoutes = require('./routes/user.routes')
+const path = require('path')
 
 const app = express()
+
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
 
 app.use(express.json())
 
 app.use('/users', userRoutes)
 
 app.get('/', (req, res) =>{
-    res.send('API Express funcionando!')
+    res.render('index')
 })
 module.exports = app
